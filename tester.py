5f1901b208cbb60d2ab6a59563461d1fb34e62ca
Shirshak Khatiwada <shirshakupadhayay182@gmail.com>
2021-06-13 13:47:04 +0545

    modified beer and teester

--- tester.py
+++ tester.py
@@ -1,3 +1,19 @@
-import greetings
 
-greetings.greeting()
+"""value=567
+complex_number=5+6j
+
+print(value.conjugate())
+print(complex_number.conjugate())"""
+
+#dict_value={} #this is dictionary if you didnt add a element inside it
+value_initial=[1,2,4,5]
+set_value={value_initial}
+value_one=[1,2,3,4,5,6]
+value_two=["josh","karen","mike","lorein"]
+value_three=[1,2,3,4,5,6]
+
+set_value.append(value_one)
+set_value.append(value_two)
+set_value.append(value_three)
+
+print(set_value)
