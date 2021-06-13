a118c3a56db1d80a15796c8e728e14a1de44caac
Shirshak Khatiwada <shirshakupadhayay182@gmail.com>
2021-06-13 23:54:23 +0545

    added flyring and vowel

---
+++ flyingCircus.py
@@ -0,0 +1,8 @@
+
+new_list=[]
+players=["shishak","hari","ram","yolo"]
+country=["france","germany","nepal"]
+new_list=[players,country]
+
+jokelist=new_list[0]
+print(jokelist[0])
---
+++ vowel.py
@@ -0,0 +1,10 @@
+
+vowels=['a','e','i','o','u']
+
+word="Shirshak"
+
+for letter in word:
+    if letter in vowels:
+        print(letter)
+    if letter not in vowels:
+        print("shrihsakupadhayay@gamil.cmc")    
